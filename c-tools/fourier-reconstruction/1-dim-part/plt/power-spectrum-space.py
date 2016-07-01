ef7b29073d82a739e4df2ccbaaf501ce9c092c72
dwille <daniel.p.willen@gmail.com>
2016-07-01 10:11:00 -0400

    Partial fix of incorrect autocorrelation in space
    In f-rec/1D-part/plt, the autocorrelation over space to find a
    wavelength was wrong. For a periodic signal, the autocorrelation should
    be periodic, however, due to the zero padding with fftconvolve, this was
    broken. This is a partial fix using fft/ifft directly

--- c-tools/fourier-reconstruction/1-dim-part/plt/power-spectrum-space.py
+++ c-tools/fourier-reconstruction/1-dim-part/plt/power-spectrum-space.py
@@ -29,7 +29,6 @@ print ""
 (partR, simdir, tstart) = simParams(sys)
 
 # Setup directory structures
-#(root, simdir, datadir, imgdir) = directoryStructureDevel(simdir)
 (root, simdir, datadir, imgdir) = directoryStructureMarcc(simdir)
 
 # Get time and z data
@@ -40,76 +39,108 @@ printSimulationData(partR, root, simdir, datadir)
 
 # Find output data -- each column is a different time
 vFracFile = datadir + "volume-fraction"
-vFrac = np.genfromtxt(vFracFile).T[:,ts:]
+vFrac = np.genfromtxt(vFracFile).T[:,tsInd:]
 
 # Autocorrelation of volume fraction
+dt = np.mean(np.diff(time))
 dz = evalZ - evalZ[0]
-freq = scifft.fftfreq(nz, np.mean(np.diff(dz)))
+deltaZ = np.mean(np.diff(dz))
+freq = scifft.fftfreq(nz, deltaZ)
 
 vfAutoCorr = np.zeros((nz, nt))
 vfPowerSpec = np.zeros((nz,nt))
-for tt, tval in enumerate(time):
-  # length of result is ceil(length(time)/2)
-  vfAutoCorr[:,tt] = AutoCorrelationFFT(vFrac[:,tt])
-  vfPowerSpec[:,tt] = np.absolute(scifft.fft(vfAutoCorr[:,tt]))**2
-
-vfPowerSpec /= (nz*nz)
-vfAutoCorrMean = np.mean(vfAutoCorr, 1)
-vfPowerSpecMean = np.mean(vfPowerSpec, 1)
-
-plt.rc('figure', figsize=(4,5))
+# for tt, tval in enumerate(time):
+#   # length of result is ceil(length(time)/2)
+#   vfAutoCorr[:,tt] = AutoCorrelationFFT(vFrac[:,tt])
+#   vfPowerSpec[:,tt] = np.absolute(scifft.fft(vfAutoCorr[:,tt]))**2
+# 
+# vfPowerSpec /= (nz*nz)
+# vfAutoCorrMean = np.mean(vfAutoCorr, 1)
+# vfPowerSpecMean = np.mean(vfPowerSpec, 1)
+# 
+# # Plot
 vfFig = plt.figure()
-vfFig.add_subplot(311)
-plt.imshow(vfAutoCorr, origin="lower", aspect="auto", interpolation="none",
-  extent=[time[0], time[-1], dz[0], dz[-1]])
+# 
+# # Plot autocorrelation (slices at constant time)
+# vfFig.add_subplot(311)
+# plt.imshow(vfAutoCorr, origin="lower", aspect="auto", interpolation="none",
+#   extent=[time[0], time[-1], dz[0], dz[-1]])
 
-vfFig.add_subplot(312)
-plt.imshow(vfPowerSpec, origin="lower", aspect="auto", interpolation="none",
-  extent=[time[0], time[-1], 0, np.max(freq)])
-#plt.plot(vfPowerSpecMean, freq, "ko:")
-plt.ylim([0, 0.05])
+vfFig.add_subplot(211)
+array = vFrac[:,0] - np.mean(vFrac[:,0])
+length = np.size(array)
+for i in np.arange(0,length):
+  array[i] = np.sin(2.*np.pi*i/length)
+plt.plot(array, np.arange(0,length), 'b')
+#plt.ylim([0, 120])
+#plt.plot(test, dz, 'k')
 
 
-#vfAutoCorr = AutoCorrelationFFT(vFrac[zs,:])
+vfFig.add_subplot(212)
+R = np.fft.ifft(np.fft.fft(array)*np.fft.fft(array[::-1]))
+plt.plot(R, np.arange(0, np.size(R)), 'k--')
 
-# Plot autocorrelation
-#vfFig.add_subplot(211)
-#plt.plot(time, vfAutoCorr)
-#plt.xlabel('Tau')
-#plt.ylabel('Autocorrelation')
-#plt.yticks([-1, -.5, 0, 0.5, 1])
-#
-vfFig.add_subplot(313)
-## Method 1
-#fftSignal = np.abs(scifft.fft(vfAutoCorr[0,:]))**2
-#W = scifft.fftfreq(nt, dt)
-#plt.plot(W, np.sqrt(fftSignal), "ko:")
-#
-tval = 1200
-# Method 2
-freqs = np.fft.fftfreq(nz, np.mean(np.diff(dz)))
-idx = np.argsort(freqs)
+# # Plot ALL power spectrums as color map
+# vfFig.add_subplot(312)
+# plt.imshow(vfPowerSpec, origin="lower", aspect="auto", interpolation="none",
+#   #extent=[time[0], time[-1], 0, np.max(freq)])
+#   extent=[time[0]-0.5*dt, time[-1]+0.5*dt, 0-0.5, np.size(freq)+0.5])
+# #plt.plot(vfPowerSpecMean, freq, "ko:")
+# plt.ylim([-1, 10])
+# plt.ylabel(r"$k$")
+# 
+# # Plot MEAN of all power spectrums averaged over time
+# vfAvgPowerSpec = np.mean(vfPowerSpec, 1)
+# 
+# vfFig.add_subplot(313)
+# plt.semilogx(vfAvgPowerSpec, np.arange(0,np.size(freq)), 'ko:')
+# plt.xlabel('PS')
+# #plt.xlim([0, 0.02])
+# plt.ylabel(r"$k$")
+# plt.ylim([0, 20])
+# plt.xlim([1e-4, 1e-1])
 
-ps = np.absolute(np.fft.fft(vfAutoCorr[:,0]))**2/(nz*nz)
-plt.plot(ps[idx], freqs[idx], "ro:")
 
-ps = np.absolute(np.fft.fft(vfAutoCorr[:,tval]))**2/(nz*nz)
-plt.plot(ps[idx], freqs[idx], "bo:")
-
-plt.legend(["t = %.2f" % time[0], "t = %.2f" % time[tval]],
-  loc="lower right")
-
-plt.xlabel("PS")
-plt.ylabel("lambda?")
-#
-plt.xlim([0, 0.02])
-plt.ylim([0, 0.05])
-
-#vfFig.add_subplot(313)
-#f, Pxx_den = signal.periodogram(vfAutoCorr, 1./dt)
-#plt.bar(f, Pxx_den)
-#plt.xlim([0,5])
-#plt.ylabel("log(sqrt(FT(autocorrelation)))")
+#  #vfAutoCorr = AutoCorrelationFFT(vFrac[zs,:])
+#  
+#  # Plot autocorrelation
+#  #vfFig.add_subplot(211)
+#  #plt.plot(time, vfAutoCorr)
+#  #plt.xlabel('Tau')
+#  #plt.ylabel('Autocorrelation')
+#  #plt.yticks([-1, -.5, 0, 0.5, 1])
+#  #
+#  vfFig.add_subplot(313)
+#  ## Method 1
+#  #fftSignal = np.abs(scifft.fft(vfAutoCorr[0,:]))**2
+#  #W = scifft.fftfreq(nt, dt)
+#  #plt.plot(W, np.sqrt(fftSignal), "ko:")
+#  #
+#  tval = 1200
+#  # Method 2
+#  freqs = np.fft.fftfreq(nz, np.mean(np.diff(dz)))
+#  idx = np.argsort(freqs)
+#  
+#  ps = np.absolute(np.fft.fft(vfAutoCorr[:,0]))**2/(nz*nz)
+#  plt.plot(ps[idx], freqs[idx], "ro:")
+#  
+#  ps = np.absolute(np.fft.fft(vfAutoCorr[:,tval]))**2/(nz*nz)
+#  plt.plot(ps[idx], freqs[idx], "bo:")
+#  
+#  plt.legend(["t = %.2f" % time[0], "t = %.2f" % time[tval]],
+#    loc="lower right")
+#  
+#  plt.xlabel("PS")
+#  plt.ylabel("lambda?")
+#  #
+#  plt.xlim([0, 0.02])
+#  plt.ylim([0, 0.05])
+#  
+#  #vfFig.add_subplot(313)
+#  #f, Pxx_den = signal.periodogram(vfAutoCorr, 1./dt)
+#  #plt.bar(f, Pxx_den)
+#  #plt.xlim([0,5])
+#  #plt.ylabel("log(sqrt(FT(autocorrelation)))")
 
 imgname = imgdir + "power-spectrum-space-vf"
 plt.savefig(imgname + ".png", bbox_inches='tight', format='png')
